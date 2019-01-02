9137ddba83f53abd64f99c7ac44501cd5cc8d05a
Matthieu Joannon <mattjoannon@gmail.com>
2019-01-02 17:06:16 +0100

    fixed lines length

--- src/components/Editor/Line.js
+++ src/components/Editor/Line.js
@@ -6,19 +6,33 @@ import PropTypes from 'prop-types'
 
 class Line extends React.Component {
   render() {
+    //We set this offset because we have to correct for the size of a station,
+    //Stations are placed from the top left hand corner but we want to place lines ends at the center of stations
+    const offset = 14
+
     return (
       <React.Fragment>
         {this.props.coords.map((coords, index) => {
           if (index !== this.props.coords.length - 1) {
+            const x1 = coords[0] + offset
+            const y1 = coords[1] + 14
+            const x2 = this.props.coords[index + 1][0] + offset
+            const y2 = this.props.coords[index + 1][1] + offset
+
+            const length = Math.sqrt(
+              Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+            )
             return (
               <line
-                x1={coords[0] + 14}
-                y1={coords[1] + 14}
-                x2={this.props.coords[index + 1][0] + 14}
-                y2={this.props.coords[index + 1][1] + 14}
+                x1={x1}
+                y1={y1}
+                x2={x2}
+                y2={y2}
                 stroke={this.props.color}
                 strokeWidth="8"
                 key={index}
+                strokeDashoffset={-10}
+                strokeDasharray={length - 20 + ' ' + 10}
               />
             )
           }
