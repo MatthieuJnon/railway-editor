9cb72b3d4901a747e4c8f121f28cae0cc3252635
Matthieu Joannon <mattjoannon@gmail.com>
2019-01-17 16:40:11 +0100

    new icons

---
+++ src/components/icons/CloseStation.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import HoverableIcon from './HoverableIcon'
+
+const CloseStation = () => {
+  return (
+    <HoverableIcon viewBox="0 0 27 27">
+      <g
+        id="Page-1"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g
+          id="Matthieu---Landing"
+          transform="translate(-485.000000, -267.000000)"
+          fillRule="nonzero"
+        >
+          <g id="delete" transform="translate(485.000000, 267.000000)">
+            <circle id="Oval" fill="#216EB4" cx="13.5" cy="13.5" r="13.5" />
+            <path
+              d="M5.4,13.5 L21.6,13.5"
+              id="Path-3"
+              stroke="#currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              transform="translate(13.500000, 13.500000) rotate(45.000000) translate(-13.500000, -13.500000) "
+            />
+            <path
+              d="M5.4,13.5 L21.6,13.5"
+              id="Path-3"
+              stroke="#currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              transform="translate(13.500000, 13.500000) rotate(-45.000000) translate(-13.500000, -13.500000) "
+            />
+          </g>
+        </g>
+      </g>
+    </HoverableIcon>
+  )
+}
+
+export default CloseStation
---
+++ src/components/icons/LinkStations.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import HoverableIcon from './HoverableIcon'
+
+const LinkStation = () => {
+  return (
+    <HoverableIcon viewBox="0 0 27 27">
+      <g
+        id="Page-1"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g
+          id="Matthieu---Landing"
+          transform="translate(-532.000000, -314.000000)"
+          fillRule="nonzero"
+        >
+          <g id="Link-" transform="translate(532.000000, 314.000000)">
+            <circle id="Oval" fill="#216EB4" cx="13.5" cy="13.5" r="13.5" />
+            <g
+              id="Group-3"
+              transform="translate(13.272727, 13.000000) rotate(-45.000000) translate(-13.272727, -13.000000) translate(3.272727, 9.000000)"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            >
+              <path
+                d="M6.72727273,6.54545455 L2.45454545,6.54545455 C0.818181818,6.54545455 0,5.45454545 0,3.27272727 C0,1.09090909 0.818181818,0 2.45454545,0 L7.72727273,0 C9.06060606,0 9.72727273,0.666666667 9.72727273,2"
+                id="Path-4"
+              />
+              <path
+                d="M16.7272727,7.54545455 L12.4545455,7.54545455 C10.8181818,7.54545455 10,6.45454545 10,4.27272727 C10,2.09090909 10.8181818,1 12.4545455,1 L17.7272727,1 C19.0606061,1 19.7272727,1.66666667 19.7272727,3"
+                id="Path-4"
+                transform="translate(14.863636, 4.272727) scale(-1, -1) translate(-14.863636, -4.272727) "
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </HoverableIcon>
+  )
+}
+
+export default LinkStation
